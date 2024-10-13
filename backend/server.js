14177e4ad78bd208c50d5380efb713a14fa2aa1a
root <root@DESKTOP-CTJPMBE.localdomain>
2024-10-12 20:59:05 -0400

    sql query

--- backend/server.js
+++ backend/server.js
@@ -92,8 +92,6 @@ app.get('/api/capacity/:hall', (req, res) => {
   } else if (endTime) {
     query += ` AND timestamp <= ?`;
     params.push(endTime);
-  } else {
-    query += ` TOP 100`
   }
 
   db.all(query, params, (err, rows) => {
